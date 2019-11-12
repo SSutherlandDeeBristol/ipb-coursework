ef84bf586b7c673c4e5712964001af78a29c0c97
SamSD <ss15060@my.bristol.ac.uk>
2019-11-12 13:04:09 +0000

    Tasty one-liner

--- sparse.py
+++ sparse.py
@@ -4,18 +4,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     mat = scipy.io.loadmat('IMAGES.mat')
-    mat = mat['IMAGES']
-
-    images = {}
+    mat = np.transpose(mat['IMAGES'], (2,0,1))
 
     for i in range(10):
-        images[i] = np.zeros((512,512))
-
-    for x in range(512):
-        for y in range(512):
-            for z in range(10):
-                (images[z])[x][y] = mat[x][y][z]
-
-    for i in range(10):
-        plt.imshow(images[i], cmap='gray')
+        plt.imshow(mat[i], cmap='gray')
         plt.show()
